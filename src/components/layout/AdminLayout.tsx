b65e6a6e092898879f0200c89c9e56611f3783f0
Chishty313 <chishty828rahman@gmail.com>
2024-07-27 17:07:02 +0600

    completed till absolute path, relative path, outlet, and layout layer

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const AdminLayout = () => {
   return (
     <div>
-      <h1>This is AdminLayout component</h1>
+      <h1>This is AdminLayout component navbar</h1>
+      <Outlet />
     </div>
   );
 };
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>This is ABOUT component.</h1>
+    </div>
+  );
+};
+
+export default About;
